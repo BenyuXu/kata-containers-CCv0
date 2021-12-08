c23ecaf5dd1dba8c5c068caddb626940407e957d
Jim Cadden <jcadden@ibm.com>
2021-12-08 10:50:33 -0500

    runtime: extend SEV detection for Ubuntu 20.04
    Signed-off-by: Jim Cadden <jcadden@ibm.com>

--- src/runtime/virtcontainers/hypervisor_amd64.go
+++ src/runtime/virtcontainers/hypervisor_amd64.go
@@ -20,7 +20,7 @@ func availableGuestProtection() (guestProtection, error) {
 	}
 	// SEV is supported and enabled when the kvm module `sev` parameter is set to `1`
 	if _, err := os.Stat(sevKvmParameterPath); err == nil {
-		if c, err := os.ReadFile(sevKvmParameterPath); err == nil && len(c) > 0 && c[0] == '1' {
+		if c, err := os.ReadFile(sevKvmParameterPath); err == nil && len(c) > 0 && (c[0] == '1' || c[0] == 'Y') {
 			return sevProtection, nil
 		}
 	}
